27721556fb974a21df1af75d4e0407b237d30313
Deeamon <marinasugutova@gmail.com>
2020-10-10 13:51:13 +0300

    feat: add crud for db

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,32 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const db = [];
+
+const getAllUsersDB = () => [...db];
+
+const saveUserToDB = user => {
+  db.push(user);
+  return 'user added!';
 };
 
-module.exports = { getAll };
+const updateUserDB = user => {
+  db.splice(
+    db.findIndex(({ id }) => id === user.id),
+    1,
+    user
+  );
+  return 'user updated!';
+};
+
+const removeUserDB = id => {
+  db.splice(
+    db.findIndex(({ user }) => user.id === id),
+    1
+  );
+  return 'user deleted!';
+};
+
+module.exports = {
+  getAllUsersDB,
+  saveUserToDB,
+  updateUserDB,
+  removeUserDB
+};
