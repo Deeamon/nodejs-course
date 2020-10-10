22f238c9e3b3da1b817a063956fb14274cd8ce34
Deeamon <marinasugutova@gmail.com>
2020-10-10 14:10:56 +0300

    feat: add routers for users

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,81 @@
 const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const { getAll, save, get, update, remove } = require('./user.service');
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  const users = await getAll();
+
+  res
+    .set('content-type', 'application/json')
+    .status(200)
+    .json(users);
+});
+
+router.route('/').post(async (req, res) => {
+  if (!req.body) {
+    res
+      .set('content-type', 'application/json')
+      .status(400)
+      .json({ message: 'Request should contains body!' });
+  } else {
+    const newUser = await save(req.body);
+    res
+      .set('content-type', 'application/json')
+      .status(200)
+      .json(newUser);
+  }
+});
+
+router.route('/:userId/').get(async (req, res) => {
+  const userId = req.params.userId;
+  const user = await get(userId);
+
+  if (user === undefined) {
+    res
+      .set('content-type', 'application/json')
+      .status(404)
+      .json({ message: `User with id ${userId} doesn't exist!` });
+  } else {
+    res
+      .set('content-type', 'application/json')
+      .status(200)
+      .json(user);
+  }
+});
+
+router.route('/:id/').put(async (req, res) => {
+  const userId = req.params.id;
+  const userData = req.body;
+  if (!(userId && userData)) {
+    res
+      .set('content-type', 'application/json')
+      .status(400)
+      .json({ message: 'Request should contains body!' });
+  } else {
+    const updatedUser = await update(userId, userData);
+
+    if (updatedUser === undefined) {
+      res
+        .set('content-type', 'application/json')
+        .status(404)
+        .json({ message: 'User not found!' });
+    } else {
+      res
+        .set('content-type', 'application/json')
+        .status(200)
+        .json(updatedUser);
+    }
+  }
+});
+
+router.route('/:id/').delete(async (req, res) => {
+  const userId = req.params.id;
+  const user = await remove(userId);
+
+  if (user === undefined) {
+    res.status(404).json({ message: `User with id ${userId} doesn't exist!` });
+  } else {
+    res.sendStatus(204);
+  }
 });
 
 module.exports = router;
