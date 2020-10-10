fab7195813a5ed6e3ffe9cc5ff978ba74cc4b5ad
Deeamon <marinasugutova@gmail.com>
2020-10-10 14:07:49 +0300

    feat: add wrapper for work with db

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,45 @@
-const usersRepo = require('./user.memory.repository');
+const {
+  getAllUsersDB,
+  saveUserToDB,
+  updateUserDB,
+  removeUserDB
+} = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const User = require('./user.model');
 
-module.exports = { getAll };
+const getAll = async () => {
+  const users = await getAllUsersDB();
+  return users.map(User.removePass);
+};
+
+const save = async userData => {
+  const newUser = await saveUserToDB(new User(userData));
+  return User.removePass(newUser);
+};
+
+const get = async id => {
+  const users = await getAll();
+  return users.find(user => user.id === id);
+};
+
+const update = async (id, data) => {
+  const user = await get(id);
+  if (user === undefined) return null;
+  return await updateUserDB(Object.assign({}, user, data));
+};
+
+const remove = async id => {
+  const user = await get(id);
+  if (user === undefined) return null;
+
+  await removeUserDB(id);
+  return user;
+};
+
+module.exports = {
+  getAll,
+  save,
+  get,
+  update,
+  remove
+};
