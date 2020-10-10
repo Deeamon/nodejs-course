752e3b91bdee668158f9138744acccb87d805ffe
Deeamon <marinasugutova@gmail.com>
2020-10-10 16:24:04 +0300

    feat: add wrapper for work with db

---
+++ src/resources/boards/board.service.js
@@ -0,0 +1,34 @@
+const {
+  getBoardsDB,
+  saveBoardToDB,
+  updateBoardDB,
+  removeBoardDB
+} = require('./board.memory.repository');
+const Board = require('./board.model');
+
+const getAllBoards = async () => await getBoardsDB();
+
+const addBoard = async boardData => await saveBoardToDB(new Board(boardData));
+
+const getBoard = async boardId =>
+  await getAllBoards().find(({ id }) => id === boardId);
+
+const updateBoard = async (boardId, boardData) => {
+  const board = await getBoard(boardId);
+  if (!board) return null;
+  return await updateBoardDB(Object.assign({}, board, boardData));
+};
+
+const removeBoard = async id => {
+  const board = await getBoard(id);
+  if (!board) return null;
+  return removeBoardDB(id);
+};
+
+module.exports = {
+  getAllBoards,
+  addBoard,
+  getBoard,
+  updateBoard,
+  removeBoard
+};
