a1139edc6a93567e23aef0368958b56f01cb182b
Deeamon <marinasugutova@gmail.com>
2020-10-10 14:11:46 +0300

    feat: add routeMap for app

--- src/app.js
+++ src/app.js
@@ -8,9 +8,7 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -19,6 +17,6 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use('/users', userRouter);
+app.use('/users/', userRouter);
 
 module.exports = app;
