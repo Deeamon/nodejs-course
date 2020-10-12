113860074bc232bc736a913ef7cc05f53be8c73f
Deeamon <marinasugutova@gmail.com>
2020-10-12 19:55:31 +0300

    feat: add wrapper for db

---
+++ src/resources/tasks/task.service.js
@@ -0,0 +1,47 @@
+const Task = require('./task.model');
+const {
+  getTasksDB,
+  saveTaskDB,
+  updateTaskDB,
+  removeTaskDB,
+  removeBoardTaskDBB
+} = require('./task.memory.repository');
+
+const getTasks = async boardId => {
+  const tasks = await getTasksDB();
+  return tasks.filter(task => task.boardId === boardId);
+};
+
+const addTask = async (boardId, taskData) =>
+  await saveTaskDB(new Task({ ...taskData, boardId }));
+
+const getTask = async (boardId, taskId) => {
+  const tasks = await getTasks(boardId);
+  if (!tasks.length) return null;
+  return tasks.find(task => task.id === taskId);
+};
+
+const updateTask = async (boardId, taskId, taskData) => {
+  const task = await getTask(boardId, taskId);
+  if (!task === undefined) return null;
+  const updatedTask = Object.assign({}, task, taskData);
+  return await updateTaskDB(updatedTask);
+};
+
+const deleteTask = async (boardId, taskId) => {
+  const task = await getTask(boardId, taskId);
+  if (!task) return null;
+
+  return removeTaskDB(taskId);
+};
+
+const removeBoardTasks = async boardId => await removeBoardTaskDBB(boardId);
+
+module.exports = {
+  getTasks,
+  addTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  removeBoardTasks
+};
